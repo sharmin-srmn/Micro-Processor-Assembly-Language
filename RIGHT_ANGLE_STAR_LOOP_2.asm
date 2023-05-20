efe7a0a12036b44f80e9fb5aa4aa3a8ff8b62ec6
Sharmin Akhter <111861975+sharmin-srmn@users.noreply.github.com>
2023-05-20 22:51:59 +0600

    Add files via upload

---
+++ RIGHT_ANGLE_STAR_LOOP_2.asm
@@ -0,0 +1,69 @@
+INCLUDE "EMU8086.INC"
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+MSG1 DB 10,13,"ENTER THE NUMBER BETWEEN 1 - 9 = $"
+NUM1 DB ?
+.CODE
+MAIN PROC 
+    
+    
+    ;MOV CX,5
+    ;MOV AH,2
+    ;MOV DL, '*'
+    
+    ;TOP:
+    ;INT 21H
+    
+    ;LOOP TOP
+    
+    MOV AX,@DATA
+    MOV DS, AX
+    
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    SUB AL,48
+    MOV NUM1,AL
+    
+    PRINTN 
+    
+    MOV BH,NUM1             ;3
+    OUTER_LOOP:             ;3>1
+    CMP BH,1
+    JL OUTER_LOOP_EXIT
+        MOV BL,BH         ;BL-3
+        INNER_LOOP:
+        CMP BL,0
+        JE INNER_LOOP_EXIT
+        
+        MOV AH,2
+        MOV DL, "*"
+        INT 21H
+        PRINT " "
+        
+        DEC BL
+        LOOP iNNER_LOOP
+        
+        INNER_LOOP_EXIT:
+        PRINTN        
+    
+    DEC BH
+    LOOP OUTER_LOOP
+    
+    OUTER_LOOP_EXIT:
+    PRINT 
+    
+     
+    
+    
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
