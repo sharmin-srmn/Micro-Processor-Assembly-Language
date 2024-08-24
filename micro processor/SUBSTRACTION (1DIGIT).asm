4907224422b1ee0a50a2c55fb6ecc4f7f9cb91e8
Sharmin Akhter <111861975+sharmin-srmn@users.noreply.github.com>
2024-08-24 21:27:59 +0600

    Update SUBSTRACTION (1DIGIT).asm

--- micro processor/SUBSTRACTION (1DIGIT).asm
+++ micro processor/SUBSTRACTION (1DIGIT).asm
@@ -7,33 +7,42 @@ INCLUDE "EMU8086.INC"
 MSG1 DW "ENTER FIRST NUMBER = $" 
 MSG2 DW "ENTER SECOND NUMBER = $" 
 MSG3 DW "THE SUBSTRACTION RESULT IS = $"
-MSG4 DW "FIRST INPUT SHOULD BE GREATER.$" 
+MSG4 DW "FIRST INPUT SHOULD BE GREATER.$"
+MSG5 DW "INVALID INPUT. ENTER NUMBER BETWEEN (0-9).$" 
 
 .CODE
 
 MAIN PROC  
     MOV AX,@DATA
-    MOV DS,AX
+    MOV DS,AX    
     
-    
-    SUBSTRACTION:    
+    OUTER:    
+    FIRST_INPUT:     
     LEA DX, MSG1  ; FIRST NUMBER MEASSAGE
     MOV AH,9
-    INT 21H
-    
+    INT 21H     
+       
     MOV AH,1      ; TAKING FIRST NUMBER INPUT
     INT 21H
     SUB AL,48
-    MOV BL,AL
-    
+    MOV BL,AL 
     
     MOV AH,2
     MOV DL,10
     INT 21H        ;NEW LINE
     MOV DL,13
-    INT 21H 
+    INT 21H     
     
+    CMP BL,0
+    JGE CHECK_LESS1
     
+    CHECK_LESS1:
+    CMP BL,9
+    JLE SECOND_INPUT    
+    
+    JMP INVALID1
+    
+    SECOND_INPUT:    
     LEA DX, MSG2
     MOV AH,9        ; SECOND NUMBER MEASSAGE
     INT 21H
@@ -43,11 +52,24 @@ MAIN PROC
     SUB AL,48        ; TAKING SECOND NUMBER INPUT
     MOV CL,AL
     
+    MOV AH,2
+    MOV DL,10
+    INT 21H        ;NEW LINE
+    MOV DL,13
+    INT 21H   
+    
+    CMP CL,0
+    JGE CHECK_LESS2
+    
+    CHECK_LESS2:
+    CMP CL,9
+    JLE SUBSTRACTION
+    
+    JMP INVALID2        
+    
+    SUBSTRACTION:    
     CMP BL,CL
-    JL INVALID
-    
-    
-     
+    JL INVALID       
     
     SUB BL,CL      ; SUBSTRACTION OPERATION
     ADD BL,48
@@ -58,9 +80,7 @@ MAIN PROC
     MOV DL,13
     INT 21H
     JMP SHOW 
-    
-    
-    
+
     SHOW:
     LEA DX, MSG3
     MOV AH,9        ; RESULT MEASSAGE
@@ -79,27 +99,41 @@ MAIN PROC
     MOV DL,13
     INT 21H 
     
-    JMP SUBSTRACTION 
+    JMP OUTER
     
-    INVALID:  
     
-    MOV AH, 2    
+    INVALID1:    
+    LEA DX, MSG5                     ; ENTER DIGIT MESSAGE FOR FIRST INPUT
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,2
     MOV DL,10
     INT 21H        ;NEW LINE
     MOV DL,13
     INT 21H
+      
+    JMP FIRST_INPUT 
     
+    
+    INVALID2:    
+    LEA DX, MSG5                     ; ENTER DIGIT MESSAGE FOR DECOND INPUT
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,10
+    INT 21H        ;NEW LINE
+    MOV DL,13
+    INT 21H
+      
+    JMP SECOND_INPUT 
+
+    INVALID:   
     LEA DX, MSG4                     ; FIRST INPUT SHOULD BE GREATER MESSAGE
     MOV AH,9
     INT 21H  
-    JMP EXIT    
-    
-    
-    ;LOOP SUBSTRACTION
-    
-    
-    
-        
+    JMP EXIT  
     
     EXIT:
     MOV AH,4CH
